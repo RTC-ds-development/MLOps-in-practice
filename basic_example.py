7e42582071ed47158ac2de442c6a4e953294f715
rcuevass <rogercuevas.ca@gmail.com>
2022-04-12 01:02:20 -0400

    add basic script and corresponding test

---
+++ basic_example.py
@@ -0,0 +1,8 @@
+
+def add_two_integers(int_1: int, int_2: int)-> int:
+    """
+    Function that takes two integers and add them up
+    """
+    int_out = int_1 + int_2
+    return int_out
+
---
+++ test_basic_example.py
@@ -0,0 +1,4 @@
+from basic_example import add_two_integers
+
+def test_add_two_integers():
+    assert 4 == add_two_integers(1,3)
