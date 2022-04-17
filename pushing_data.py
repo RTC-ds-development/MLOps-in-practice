964da9ed509d18f4260cb3a7b087b0fd32c2aa18
rcuevass <rogercuevas.ca@gmail.com>
2022-04-17 15:24:25 -0400

    testing csv file exists

--- pushing_data.py
+++ pushing_data.py
@@ -1,6 +1,11 @@
 import pandas as pd
+from os.path import exists
 
 d = {'col_1':[1,2], 'col_2':[3,4]}
 df = pd.DataFrame(data=d)
 
 df.to_csv('test_data.csv')
+
+file_exists = exists('test_data.csv')
+
+print(file_exists)
